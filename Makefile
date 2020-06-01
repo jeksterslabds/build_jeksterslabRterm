259d391ee5e5e4b1301ee6b88a62b8cebe4a2410
Ivan Jacob Agaloos Pesigan <learn.jeksterslab@gmail.com>
2020-06-01 12:23:49 +0800

    Update.

--- Makefile
+++
@@ -1,8 +0,0 @@
-.PHONY: all clean
-
-all :
-	Rscript "deploy.R"
-	Rscript -e 'rmarkdown::render("deploy.R")'
-
-clean :
-	Rscript reset_user_lib.R
--- reset_user_lib.R
+++
@@ -1,4 +0,0 @@
-source("https://raw.githubusercontent.com/jeksterslabds/jeksterslabRutils/master/R/util_txt2file.R")
-source("https://raw.githubusercontent.com/jeksterslabds/jeksterslabRterm/master/R/term_user_lib.R")
-source("https://raw.githubusercontent.com/jeksterslabds/jeksterslabRterm/master/R/term_reset_user_lib.R")
-term_reset_user_lib()
